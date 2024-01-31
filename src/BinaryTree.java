10c59a9fca3990ecd060ccf2ed2769772eea392e
Shawn Hancock <90814768+Shawn-Hanclock@users.noreply.github.com>
2024-01-30 22:00:21 -0800

    feat: Added constructor and addRoot for Node, BinaryTree

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,11 +1,30 @@
 public class BinaryTree
 {
     // variables
+    private Node root;
     // root node
     // current node
 
     // constructor
-    //
+    public BinaryTree()
+    {
+        root = null;
+    }
+    public void addDataRoot(int data)
+    {
+        if(root == null) {
+            root = new Node(data);
+        }
+        else {
+            root.setData(data);
+        }
+    }
+    public void addData(int data, boolean left)
+    {
+
+    }
+
+
 
     // population methods
     // insert element
--- src/Node.java
+++ src/Node.java
@@ -1,6 +1,18 @@
 public class Node
 {
+    private int data;
+    private Node left;
+    private Node right;
     //data
     //pointer left and right
+    public Node(int data)
+    {
+        this.data = data;
+        left = null;
+        right = null;
+    }
 
+    public void setData(int data) {
+        this.data = data;
+    }
 }
