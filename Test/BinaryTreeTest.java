9ee78b19df051726ba45a27c1bf250b9af099fb7
svh <nwahs007@gmail.com>
2024-02-22 23:27:07 -0800

    Created blank jUnit tests.

---
+++ Test/BinaryTreeTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinaryTreeTest {
+
+    @Test
+    void getRoot() {
+    }
+
+    @Test
+    void treeLeaf() {
+    }
+
+    @Test
+    void addData() {
+    }
+
+    @Test
+    void testAddData() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
