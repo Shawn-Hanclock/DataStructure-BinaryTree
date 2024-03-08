0eb4a562d3a132a6c9f11a2a03a672cf4475c173
Shawn-Hanclock <nwahs007@gmail.com>
2024-03-08 13:18:24 -0800

    Unit tested an empty binary tree, and unit tested another non empty.

--- Test/BinaryTreeTest.java
+++ Test/BinaryTreeTest.java
@@ -3,6 +3,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryTreeTest {
     BinaryTree empty = new BinaryTree();
+    BinaryTree tree = new BinaryTree(1);
+    BinaryTree treeArr = new BinaryTree();
     @Test
     void emptyToStringPass()
     {
@@ -22,13 +24,6 @@ class BinaryTreeTest {
         assertTrue(str.equals("[0]"));
     }
     @Test
-    void emptyAddDataFail()
-    {
-        empty.addData(0.2);
-        String str = empty.toString();
-        assertTrue(str.equals("[0]"));
-    }
-    @Test
     void emptyGetRootDataPass()
     {
         empty.addData(0);
@@ -41,4 +36,62 @@ class BinaryTreeTest {
         int leafs = empty.treeLeaf();
         assertEquals(1, leafs);
     }
+    @Test
+    void treeToStringPass()
+    {
+        assertEquals("[1]", tree.toString());
+    }
+    @Test
+    void treeAddDataPass()
+    {
+        tree.addData(0);
+        assertEquals("[0, 1]" , tree.toString());
+    }
+    @Test
+    void treeAddDataTwicePass()
+    {
+        tree.addData(0);
+        tree.addData(-1);
+        assertEquals("[0, 1, -1]" , tree.toString());
+    }
+    @Test
+    void treeRootDataPass()
+    {
+        Node root = tree.getRoot();
+        assertEquals(root.getData(), 1);
+    }
+    @Test
+    void treeDefaultLeafsPass()
+    {
+        int leaf = tree.treeLeaf();
+        assertTrue(leaf == 1);
+    }
+    @Test
+    void treeDefaultLeafsFail()
+    {
+        int leaf = tree.treeLeaf();
+        assertFalse(leaf == 2);
+    }
+    @Test
+    void treeAddedLeafsPass()
+    {
+        int n = 4;
+        for(int i = 0; i <= n; i++) {
+         tree.addData(i);
+        }
+        int N = n+1;
+        int expected = (N + 1) / 2;
+        assertEquals(expected, tree.treeLeaf());
+    }
+    @Test
+    void treeAddedToStringPass()
+    {
+        int n = 5;
+        for(int i = 2; i <= n; i++) {
+         tree.addData(i);
+        }
+        String expected = "[4, 2, 5, 1, 3]";
+        assertEquals(expected, tree.toString());
+    }
+
 }
